1a6a2ceb16872f6e22a6ace76b91e2f40996f2a9
jbergen <jbergen@gmail.com>
2019-04-07 22:27:57 -0400

    updated single page css for mobile

--- single.php
+++ single.php
@@ -1,15 +1,15 @@
 <?php get_header() ?>
 <?php the_post() ?>
 
-<div class='span16 single-content'>
+<div class='container single-content'>
 	<h1><?php the_title(); ?></h1>
 	<div class='row'>
 		
-		<div class="col-8 readable">
+		<div class="col-sm-9 readable">
 			<?php the_content() ?>
 		</div>
 
-		<div class="col-4 sidebar">
+		<div class="col-sm-3 sidebar">
 			<?php get_sidebar() ?>
 		</div>
 		
